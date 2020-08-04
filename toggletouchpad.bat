f8b905e7176fff09efde62a0dfcb4bea5c9a4da9
Harry Bond <smashedchant@gmail.com>
2020-08-04 04:40:38 +0100

    script works but the key is only to keep track not to change :(
    need to find the right key now or a different approach

--- toggletouchpad.bat
+++ toggletouchpad.bat
@@ -1,10 +1,7 @@
 @echo off &setlocal
 
-for /f "tokens=2*" %%a in ('REG QUERY HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\PrecisionTouchPad\Test /v Variable') do set "var=%%b"
+for /f "tokens=2*" %%a in ('REG QUERY HKCU\Software\Microsoft\Windows\CurrentVersion\PrecisionTouchPad\Status /v Enabled') do set "var=%%b"
 if "%var%"=="0x1" (set out=0x0) else set out=0x1
 echo %out%
-REG ADD HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\PrecisionTouchPad\Test /v Variable /t REG_DWORD /d %out% /f
+REG ADD HKCU\Software\Microsoft\Windows\CurrentVersion\PrecisionTouchPad\Status /v Enabled /t REG_DWORD /d %out% /f
 PAUSE
-
-rem reg add HKCU\Software\Microsoft\Windows\CurrentVersion\PrecisionTouchPad\Status /v Enabled /d 0x0 /f
-rem reg query HKCU\Software\Microsoft\Windows\CurrentVersion\PrecisionTouchPad\Status /v Enabled

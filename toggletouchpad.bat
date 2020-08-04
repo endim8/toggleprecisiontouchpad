a892d25ef67c877ca79b6b24e507ef0323f16598
Harry Bond <smashedchant@gmail.com>
2020-08-04 02:52:10 +0100

    registry to variable

--- toggletouchpad.bat
+++ toggletouchpad.bat
@@ -1,3 +1,4 @@
-@echo off
-reg query HKCU\Software\Microsoft\Windows\CurrentVersion\PrecisionTouchPad\Status /v Enabled
+@echo off &setlocal
+for /f "tokens=2*" %%a in ('reg query HKCU\Software\Microsoft\Windows\CurrentVersion\PrecisionTouchPad\Status /v Enabled') do set "var=%%b"
+if "%var%"=="0x1" (echo 0x1) else echo no
 PAUSE
